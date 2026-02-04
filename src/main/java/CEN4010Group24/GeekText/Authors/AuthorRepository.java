87568ec33f8930941bd36a9960fba4215c3185a9
Jennifer Ward <jenniferward@Jennifers-MacBook-Pro.local>
2026-02-03 20:25:21 -0500

    Keep only setup-db-docker in branch

--- src/main/java/CEN4010Group24/GeekText/Authors/AuthorRepository.java
+++
@@ -1,8 +0,0 @@
-package CEN4010Group24.GeekText.Authors;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AuthorRepository extends JpaRepository<Author, Long> {
-}
